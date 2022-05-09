5985ce92c2dfce6482da3ee4f976d4066fbb5e41
meidricsq <41635972+meidricsq@users.noreply.github.com>
2022-05-09 15:50:30 +0300

    Added Exchange Parts Event

--- server/main.lua
+++ server/main.lua
@@ -6,3 +6,15 @@ RegisterNetEvent("mdx-chopshop:server:getItems", function(item, amount)
     TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[item], "add", amount)
 end)
 
+RegisterNetEvent("mdx-chopshop:server:ExchangeItems", function(itemremove, item, amountremove, amount)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.Functions.GetItemByName(itemremove) then
+        Player.Functions.RemoveItem(itemremove, amountremove)
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[itemremove], "remove", amountremove)
+        Player.Functions.AddItem(item, amount)
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[item], "add", amount)
+    else
+        TriggerClientEvent("QBCore:Notify", source, "You do not have enough of that item.", "error")
+    end
+end)
+
